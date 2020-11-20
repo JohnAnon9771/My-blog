2613e3e884add4d94a0d7342e07d955aa2c136ad
JohnAnon9771 <njoao97710@gmail.com>
2020-11-20 13:42:11 -0300

    :lipstick: STYLE: added config for themes

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,30 @@
+const colors = {
+  primary: '#A9CEF4',
+  categories: '#A9CEF4',
+  white: '#FFFFFF',
+  text: {
+    primary: '#4433FF',
+    secondary: '#FF0A78',
+    black: '#00000',
+  },
+};
+
+// target / context = result
+const textVariants = {
+  title: '1rem',
+  subTitle: '0.87rem',
+  lxTitle: '1.125rem',
+};
+
+const borderRadii = {
+  s: '5px',
+  m: '10px',
+  l: '15px',
+  xl: '25px',
+};
+
+export const theme = {
+  colors,
+  textVariants,
+  borderRadii,
+};
